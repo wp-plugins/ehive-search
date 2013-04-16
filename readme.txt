e092783a9507dc5cd24420a6734023b92a5dd64d
vernonsystems <vernonsystems@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-04-16 02:13:58 +0000

    First stable release
    git-svn-id: https://plugins.svn.wordpress.org/ehive-search/trunk@698212 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,122 @@
+=== eHive Search ===
+Contributors: vernonsystems 
+Donate link:http://ehive.com/what_is_ehive
+Tags: ehive, collection, museum, archive, history
+Requires at least: 3.3.1
+Tested up to: 3.5.1
+Stable tag: 3.5.1
+License: GPL2+
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+A plugin that give you the power to search eHive Objects from your WordPress website.
+
+== Description ==
+
+This plugin is part of a suite of plugins created by Vernon Systems Ltd., which give you the power to embed eHive functionality into your WordPress website.
+
+This plugin gives you the ability to add eHive search functionality to your website. When added to your site the eHive Search plugin allows your site's visitors to search for eHive Objects. Search results can be displayed as a list view, lightbox view or both. You can optionally add an account and/or community filter in the eHive Access plugin's options page so that your site returns Object Records from the given account and/or community only.
+
+You can configure the search options to display your choice of fields for the search results summary.
+
+Before you install this plugin you will need to install the eHive Access plugin. 
+
+<span style="text-decoration: underline;">**Get more from the eHive plugin suite**</span>
+
+To enhance the experience you offer your users you can also install the eHive Object details plugin to allow your users to click all the way through to view the Object Records in detail. Furthermore, you can add the eHive Search widget and give your visitors the option to search eHive on every page, not just the page where you have added the plugin's shortcode.
+
+While eHive search will function and return results without the eHive Object Details plugin, it is likely that you will want to install the eHive Object Details plugin so your users can click through and view an Object Record in detail.
+
+Other plugins in the suite include:
+
+* eHive Account Details - A plugin for displaying eHive account information.
+* eHive Object Comments - Enables users to add comments to Object Records from your site.
+* eHive Object Details - A plugin for displaying Object Record detail pages.
+* eHive Objects Image Grid - Displays a grid of images from eHive filtered by certain criteria.
+* eHive Objects Tag Cloud - Displays a tag cloud from eHive.
+* eHive Objects Gallery widget - Provides a gallery of Object Records that can be placed in your sites widget areas. 
+* eHive Object Tags widget - A widget that displays tags for an Object Record.
+* eHive Objects Tag Cloud widget - Allows you to display a tag cloud in a widget area on your site.
+* eHive Search widget - A widget plugin that provides access to eHive Search from a widget.
+
+<div>
+  <br />
+</div>
+
+
+== Installation ==
+**Dependencies:**
+
+* eHive Access plugin
+* eHive Object Details plugin (optional but strongly advised)
+
+This plugin requires the eHive Access plugin to be installed first. Please ensure you have installed and configured these plugins correctly first before installing this plugin.
+
+There are three ways to install a plugin:
+
+<span style="text-decoration: underline;">**Method 1**</span>
+
+
+1.  Navigate to the plugins page by clicking the link in the WordPress admin menu.
+2.  Click the "Add New" link
+3.  Type the name of the plugin you want to install (i.e. "eHive Access plugin") into the search box
+4.  Click the "Search plugins" button
+5.  Locate the plugin you want to install from the search results
+6.  Click the "Install Now" link and click "OK" on the popup confirmation window
+7.  Click the "Activate plugin" link when the plugin installation has completed
+
+
+<span style="text-decoration: underline;">**Method 2**</span>
+
+
+1.  Download the plugin's .ZIP file.
+2.  Navigate to the plugins page by clicking the link in the WordPress admin menu.
+3.  Click the "Add New" link
+4.  Click the "Upload" link
+5.  Click the "Choose File" button and locate the .ZIP file you downloaded in step 1
+6.  Click the "Install Now" button
+7.  Click the "Activate plugin" link when the plugin installation has completed
+
+<span style="text-decoration: underline;">**Method 3**</span>
+
+
+1. Download the plugin's .ZIP file.
+2. Unzip the contents into your WordPress sites plugin directory (<em>/wordpress/wp-contents/plugins</em>)
+3. Navigate to the plugins page by clicking the link in the WordPress admin menu.
+4. Click the "Activate plugin" link below the plugin's name
+
+== Changelog ==
+= 2.1.1 =
+* First stable release of the eHive Access plugin. 
+
+== Upgrade Notice ==
+= 2.1.1 =
+This is the first stable release of the eHive Access plugin.
+
+== Screenshots ==
+
+1. screenshot_1.png
+2. screenshot_2.png
+
+== Frequently Asked Questions ==
+
+Q. What is eHive?
+
+A. eHive is an online collections management software package. See more at <a href="http://ehive.com/what_is_ehive" target="_blank" title="what is ehive?">What is eHive?</a>
+
+<div>
+	<br />
+</div>
+
+Q. What do these plugins do?
+
+A. The eHive plugin suite gives you the ability to provide eHive functionality to your site's visitors. This means that you can search and display eHive records, leave comments that are visible also in ehive and add and remove tags to records where the record owner has given permission to do so. You can filter the search results by account or community if you want to display only records from a particular source. We also provide plugins to do other nice things like display grids of interesting, popular or recent images added to eHive; display galleries of other objects by the same account as a record you are viewing etc.
+
+<div>
+	<br />
+</div>
+
+Q. How do I get an API Key?
+
+A. To get an API Key you will first need an active eHive account. If you don't have one you can <a href="http://ehive.com/signup/" title="sign up for an ehive account">sign up</a> for an account for free. Once you have an account you can navigate to the "Edit My Profile > Api Keys" page and create a new Key.
+
+
---
+++ templates/eHiveSearch.php
@@ -0,0 +1,348 @@
+<?php
+/*
+	Copyright (C) 2012 Vernon Systems Limited
+
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+if ($css_class == "") {
+	echo '<div class="ehive-search">';
+} else {
+	echo '<div class="ehive-search '.$css_class.'">';
+}
+
+if ($options['hide_search_form_enabled'] != 'on') {
+	echo '<form class="ehive-search" name="ehive-search-form" action="'. $eHiveAccess->getSearchPageLink() .'" method="get">';
+		echo '<input class="ehive-query" type="text" name="'. $options['query_var'] .'" value="'. ehive_get_var($options['query_var']).'"/>';
+		echo '<input class="ehive-submit" type="submit" value="Search"/>';
+	echo '</form>';
+}
+
+if (!isset($eHiveApiErrorMessage)) {
+	if (isset($objectRecordsCollection)) {	
+		
+		if ($objectRecordsCollection->totalObjects > 0) {
+			echo '<div class="ehive-search-results">';				
+				$view = ehive_get_var('view', $resultsViewDefault);		
+				echo '<div class="ehive-navigation">';
+					echo '<div class="ehive-pagination">';		
+						if ( $a ) {
+							$all = ($queryAll==true) ? '&all=true' : '';
+							
+							$pBase = ehive_link2('Search') . '?a='.$a.$all.'&view='.$view.'&%_%';
+							$pFormat = $options['page_var'] . '=%#%';
+							$pTotal = ceil($objectRecordsCollection->totalObjects / $options['limit']);
+							$pCurrent = ehive_get_var($options['page_var'], 1);
+							
+							echo paginate_links( array('base' => $pBase, 'format' => $pFormat, 'total' => $pTotal, 'current' => $pCurrent) );
+						
+						} else {		
+							$all = ($queryAll==true) ? '&all=true' : '';
+									
+							$pBase = ehive_link2('Search') . '?' . $options['query_var'] . '=' . urlencode(utf8_encode( ehive_get_var($options['query_var']) )) .$all.'&view='.$view.'&%_%';
+							$pFormat = $options['page_var'] . '=%#%';
+							$pTotal = ceil($objectRecordsCollection->totalObjects / $options['limit']);
+							$pCurrent = ehive_get_var($options['page_var'], 1);
+							echo paginate_links( array('base' => $pBase, 'format' => $pFormat, 'total' => $pTotal, 'current' => $pCurrent) );
+						} 	
+						
+					echo '</div>';				
+					resultsViewNavigation($options, $resultsViewDefault, $resultsViewLightboxEnabled, $resultsViewListEnabled, $resultsViewPosterboardEnabled);				
+				echo '</div>';
+						
+				$imageSize = "";
+				$itemInlineStyleEnabled = false;
+				$imageInlineStyleEnabled = false;
+				$itemInlineStyle = '';
+				$imageInlineStyle = '';
+				$lightboxColumns = $options['lightbox_columns'];
+				
+				if ($view == 'lightbox') {							
+					$imageSize = "s";
+					
+					if (isset($options['plugin_css_enabled']) && isset($lightboxColumns) && $lightboxColumns > 0) {
+						$sectionWidth = 100.0/$lightboxColumns;
+						$sectionMargin = $sectionWidth/($lightboxColumns*$lightboxColumns);
+						$sectionWidth = $sectionWidth - $sectionMargin - 1.40;
+					
+						$itemInlineStyle .= "max-width: $sectionWidth%; width: $sectionWidth%; margin-right: $sectionMargin%; ";
+						$itemInlineStyleEnabled = true;
+					}
+					
+				} else {
+					$imageSize = "ts";
+				}
+			
+				if ($options['item_background_colour_enabled'] == 'on') {
+					$itemInlineStyle .= "background:$item_background_colour; ";
+					$itemInlineStyleEnabled = true;
+				}
+				if ($options['item_border_colour_enabled'] == 'on' && $item_border_width > 0) {
+					$itemInlineStyle .= "border-style:solid; border-color:$item_border_colour; ";
+					$itemInlineStyle .= "border-width:{$item_border_width}px; *margin:-{$item_border_width}px; ";
+					$itemInlineStyleEnabled = true;
+				}
+				if ($options['image_background_colour_enabled'] == 'on') {
+					$imageInlineStyle .= "background:$image_background_colour; ";
+					$imageInlineStyleEnabled = true;
+				}
+				if ($options['image_padding_enabled'] == 'on') {
+					$imageInlineStyle .= "padding:{$image_padding}px; ";
+					$imageInlineStyleEnabled = true;
+				}
+				if ($options['image_border_colour_enabled'] == 'on' && $image_border_width > 0) {
+					$imageInlineStyle .= "border-style:solid; border-color:$image_border_colour; ";
+					$imageInlineStyle .= "border-width:{$image_border_width}px; ";
+					$imageInlineStyleEnabled = true;
+				}
+				
+				if($itemInlineStyleEnabled) {
+					$itemInlineStyle = " style='$itemInlineStyle'";
+				}
+				if($imageInlineStyleEnabled) {
+					$imageInlineStyle = " style='$imageInlineStyle'";
+				}
+				
+				echo "<div class='ehive-view ehive-$view'>";		
+					foreach ($objectRecordsCollection->objectRecords as $objectRecord) {
+						echo "<div class='ehive-item' $itemInlineStyle>";
+						
+							echo '<div class="ehive-item-image-wrap">';
+								echo '<a class="ehive-image-link" href="'.$eHiveAccess->getObjectDetailsPageLink($objectRecord->objectRecordId).'">';
+								
+									$imageLink = '<img class="ehive-image" src="/wp-content/plugins/eHiveSearch/images/no_image_ts.png" alt="'.$objectRecord->name.'" title="'.$objectRecord->name.'" >';
+									
+									$imageMediaSet = $objectRecord->getMediaSetByIdentifier('image');
+									if (isset($imageMediaSet)) {
+										$mediaRow = $imageMediaSet->mediaRows[0];
+										$imageMedia = $mediaRow->getMediaByIdentifier("image_$imageSize");
+										$imageLink = '<img class="ehive-image" src="'.$imageMedia->getMediaAttribute('url').'"' . $imageInlineStyle . ' alt="'.$imageMedia->getMediaAttribute('title').'" title="'.$imageMedia->getMediaAttribute('title').'">';
+									}
+									
+									echo $imageLink;
+								echo '</a>';
+							echo '</div>';
+						
+							echo '<div class="ehive-item-metadata-wrap">';
+								if ($view == 'lightbox') {
+									lightboxMetadata( $options, $objectRecord, $eHiveAccess );
+								} else {
+									listMetadata( $options, $objectRecord, $eHiveAccess, $eHiveApi );
+								}
+							echo '</div>';						
+						echo '</div>';
+					}
+				echo '</div>';
+			
+			if ( $poweredByEhiveEnabled ) {
+				echo '<a href="http://ehive.com/what-is-ehive" target="_blank"><img class="ehive-logo-powered-by" src="/wp-content/plugins/eHiveSearch/images/powered_by_ehive_small.png" alt="Powered by eHive" title="Powered by eHive" width="103" height="35"></a>';
+			}
+			echo '</div>';
+								
+		}  else {
+			echo '<p class="ehive-no-results">There were no results found</p>';
+		}
+	} 
+} else {
+	echo "<p class='ehive-error-message ehive-account-details-error'>$eHiveApiErrorMessage</p>";	
+}
+echo '</div>';
+
+
+function resultsViewNavigation($options, $resultsViewDefault, $resultsViewLightboxEnabled, $resultsViewListEnabled, $resultsViewPosterboardEnabled) {
+	
+	$view = ehive_get_var('view', $resultsViewDefault);	
+	
+	$queryAll = ehive_get_var('all', false);
+	$all = ($queryAll==true) ? '&all=true' : '';
+		
+	$currentPage = '';
+	$pageNumber = ehive_get_var($options['page_var']);
+	if (isset($options['page_var']) && isset($pageNumber) && $pageNumber > 1) {
+		$currentPage.="&{$options['page_var']}=$pageNumber";
+	}
+	
+	$link = ehive_link2('Search').'?'. $options['query_var'].'='.urlencode(utf8_encode( ehive_get_var($options['query_var']))).$all.$currentPage;	
+
+	if ($resultsViewLightboxEnabled && $resultsViewListEnabled) { 			
+		
+		echo '<ul class="ehive-view-navigation">';		
+		echo '<li>View by:</li>';
+				
+		if ($resultsViewLightboxEnabled) {
+			if ($view == 'lightbox') {
+				echo '<li>lightbox</li>';
+			} else {
+				echo '<li><a href="'.$link.'&view=lightbox'.'">lightbox</a></li>';
+			}
+		}			
+		
+		if ($resultsViewListEnabled) {
+			if ($view == 'list') {
+				echo '<li>list</li>';
+			} else {
+				echo '<li><a href="'.$link.'&view=list'.'">list</a></li>';
+			}
+		}
+						
+		echo '</ul>';
+	}
+}
+
+function lightboxMetadata($options, $objectRecord, $eHiveAccess) {
+	$items = array(	'object_number' => 'object_number',
+					'name' => 'name',
+					'primary_creator_maker' => 'primary_creator_maker',
+					'primary_creator_maker_role' => 'primary_creator_maker',
+					'taxonomic_classification' => 'taxon',
+					'taxonomic_type_indicator' => 'taxon',
+					'field_collector' => 'field_collector',
+					'web_public_description' => 'web_public_description',
+					'date_made' => 'date_made',
+					'place_made' => 'place_made',
+					'object_type' => 'object_type',
+					'medium_description' => 'medium_description',
+					'measurement_description' => 'measurement_description',
+					'named_collection' => 'named_collection',
+					'credit_line' => 'credit_line'
+	);
+		
+	$enabledFields = array();
+	
+	foreach($items as $key => $value) {
+		$fieldLabel = null;
+		$fieldValue = null;
+		
+		
+		if ($options["lightbox_{$key}_enabled"] == 'on') {
+			$fieldSet = $objectRecord->getFieldSetByIdentifier($value);
+			if (isset($fieldSet)) {
+				$fieldRow = $fieldSet->fieldRows[0];
+				$field = $fieldRow->getFieldByIdentifier($key);
+				
+				if (isset($field) && $field != null) {
+					$fieldValue = $field->getFieldAttribute('value');
+				} else {
+					continue;
+				}
+			}
+			$fieldLabel = $options["lightbox_{$key}_enabled_label"];
+		}
+		if (  isset($fieldValue) && $fieldValue !='' ) {		
+			echo '<p class="ehive-field ehive-identifier-'.$key.'">';
+			if (isset($fieldLabel) && $fieldLabel !='') {
+				echo '<span class="ehive-field-label">'.$fieldLabel.'</span>';
+			}
+			if (isset($fieldValue) && $fieldValue !='') {
+				echo $fieldValue;
+			}
+			echo '</p>';
+		}	
+	}
+	if ($options['lightbox_more_link_enabled'] == "on") {
+		echo '<a class="ehive-more-link" href="'.$eHiveAccess->getObjectDetailsPageLink($objectRecord->objectRecordId).'">'.$options['lightbox_more_link_text'].'</a>';
+	}
+}
+
+function listMetadata($options, $objectRecord, $eHiveAccess, $eHiveApi) {
+	$showPublicProfileName = $options['show_public_profile_name'] == 'on' ? true : false;
+	$showCatalogueTypeIcon = $options['show_catalogue_type_icon'] == 'on' ? true : false;
+	
+	echo '<a href="'.$eHiveAccess->getObjectDetailsPageLink($objectRecord->objectRecordId).'"><span class="ehive-item-summary">'.listFields($options, $objectRecord).'</span></a>';
+		
+	if ( $showPublicProfileName ) {
+		echo '<p class="ehive-field ehive-identifier-public_profile_name">';
+		echo '<span class="ehive-field-label">From: </span>';
+		echo '<a href="'.$eHiveAccess->getAccountDetailsPageLink($objectRecord->accountId).'">'.$eHiveApi->getAccount($objectRecord->accountId)->publicProfileName.'</a>';
+		echo '</p>';
+	}
+
+	if ( $showCatalogueTypeIcon ) {
+		switch ($objectRecord->catalogueType) {
+			case 'archaeology':
+				echo '<img class="ehive-catalogue-type ehive-archaeology" src="/wp-content/plugins/eHiveSearch/images/catalogue_archaeology.png" width="21" height="21" alt="Archaeology" title="Archaeology"/>';
+				break;
+			case 'archives':
+				echo '<img class="ehive-catalogue-type ehive-archives" src="/wp-content/plugins/eHiveSearch/images/catalogue_archives.png" width="21" height="21" alt="Archives" title="Archives"/>';
+				break;
+			case 'art':
+				echo '<img class="ehive-catalogue-type ehive-art" src="/wp-content/plugins/eHiveSearch/images/catalogue_art.png" width="21" height="21" alt="Art" title="Art"/>';
+				break;
+			case 'history':
+				echo '<img class="ehive-catalogue-type ehive-history" src="/wp-content/plugins/eHiveSearch/images/catalogue_history.png" width="21" height="21" alt="History" title="History"/>';
+				break;
+			case 'library':
+				echo '<img class="ehive-catalogue-type ehive-library" src="/wp-content/plugins/eHiveSearch/images/catalogue_library.png" width="21" height="21" alt="Library" title="Library"/>';
+				break;
+			case 'natural_science':
+				echo '<img class="ehive-catalogue-type ehive-natural-science" src="/wp-content/plugins/eHiveSearch/images/catalogue_natural_science.png" width="21" height="21" alt="Natural Science" title="Natural Science"/>';
+				break;
+			case 'photography':
+				echo '<img class="ehive-catalogue-type ehive-photography" src="/wp-content/plugins/eHiveSearch/images/catalogue_photography.png" width="21" height="21" alt="Photography" title="Photography"/>';
+				break;
+		}
+	}
+
+}
+
+function listFields($options, $objectRecord) {
+		
+	$items = array(	'object_number' => 'object_number',
+					'name' => 'name',
+					'primary_creator_maker' => 'primary_creator_maker',
+					'primary_creator_maker_role' => 'primary_creator_maker',
+					'taxonomic_classification' => 'taxon',
+					'taxonomic_type_indicator' => 'taxon',
+					'field_collector' => 'field_collector',
+					'web_public_description' => 'web_public_description',
+					'date_made' => 'date_made',
+					'place_made' => 'place_made',
+					'object_type' => 'object_type',
+					'medium_description' => 'medium_description',
+					'measurement_description' => 'measurement_description',
+					'named_collection' => 'named_collection',
+					'credit_line' => 'credit_line'
+	);
+	
+	$summary = '';
+	
+	foreach($items as $key => $value) {
+		$fieldLabel = null;
+		$fieldValue = null;
+	
+		if ($options["list_{$key}_enabled"] == 'on') {
+			$fieldSet = $objectRecord->getFieldSetByIdentifier($value);
+			if (isset($fieldSet)) {
+				$fieldRow = $fieldSet->fieldRows[0];
+				$field = $fieldRow->getFieldByIdentifier($key);
+				
+				if (isset($field) && $field != null) { 
+					$fieldValue = $field->getFieldAttribute('value');
+				} else {
+					continue;
+				}
+			}
+			if ($fieldValue != '') {
+				if ($summary != '') {
+					$summary = $summary.'; '.$fieldValue;
+				} else {
+					$summary = $fieldValue;
+				}
+			} 
+		}		
+	}
+	return $summary;
+}
+
+?>
